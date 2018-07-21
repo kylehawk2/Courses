aecdf55c86ad9c26ceb435aa3947400c79f0ebd4
Kyle <kylehawk2@gmail.com>
2018-07-21 09:27:48 -0700

    Fisrt Commit

---
+++ main/apps/courses/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return "<Course object: {} {}>".format(self.name, self.desc)

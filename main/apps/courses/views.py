15730a3b0e74064aab5de9c60765acee76afec9c
unknown <kylehawk2@gmail.com>
2018-07-21 14:19:11 -0700

    First Commit

--- main/apps/courses/views.py
+++ main/apps/courses/views.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from models import Course
+from django.contrib import messages
 
 def index(request):
-    return render(request, 'courses/index.html')
+    return render(request, 'courses/index.html', { "course": Course.objects.all() })
+
+def destroy(request, id):
+    return render(request, 'courses/destroy.html')

773bd6ac45593b89ac4afd2126c06671092dca61
Kyle <kylehawk2@gmail.com>
2018-07-21 15:39:53 -0700

    First Commit

--- main/apps/courses/views.py
+++ main/apps/courses/views.py
@@ -6,7 +6,7 @@ from models import Course
 from django.contrib import messages
 
 def index(request):
-    return render(request, 'courses/index.html', { "course": Course.objects.all() })
+    return render(request, 'courses/index.html', { "courses": Course.objects.all() })
 
 def destroy(request, id):
-    return render(request, 'courses/destroy.html')
+    return render(request, 'courses/destroy.html', {"courses": Course.objects.get(id=int(id))})
